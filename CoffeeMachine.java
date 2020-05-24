210a27060f3ca0d079e2e6f25633a6586003a84b
Heramb Jaiswal <jaiswalheramb30@gmail.com>
2020-05-24 17:14:26 +0530

    with some more new way to assist user...

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,5 +1,6 @@
     package machine;
 import java.util.Scanner;
+import java.lang.*;
 public class CoffeeMachine {
     public static void main(String[] args) {
         System.out.println("Starting to make a coffee\nGrinding coffee beans\nBoiling water\nMixing boiled water with crushed coffee beans\nPouring coffee into the cup\nPouring some milk into the cup\nCoffee is ready!");
@@ -12,70 +13,31 @@ public class CoffeeMachine {
       String action;
       
       
+      while(true)
+      {
+      System.out.println("Write action (buy, fill, take, remaining, exit):");
+      action=sc.next();
+       
+       switch(action)
+       { 
+           
       
+      
+       case "remaining" :
           System.out.println("The coffee machine has");
           System.out.println(water+" of water\n"+milk+" of milk\n"+bean+" of coffee beans\n"+cup+" of disposable cups\n"+money+" of money");
-           
+           break;
            
     
-      System.out.println("Write action (buy, fill, take):");
-           action=sc.next();
-          if(action.equals("buy"))
-          {
-              System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
-              int num=sc.nextInt();
-              if(num==1)
-              {
-                  if(water<250 || bean<16)
-                  {
-                      System.out.println("sorry i have insufficient integrident");
-                  }
-                  else
-                  {
-                     water=water-250;
-                     bean=bean-16;
-                     money=money+4; 
-                     cup=cup-1;
-                  }
-                  
-              }
-              else if(num==2)
-              {
-                  if(water<350 || bean<20 || milk<75)
-                  {
-                      System.out.println("sorry i have insufficient integrident");
-                  }
-                  else
-                  {
-                    
-                     water=water-350;
-                     bean=bean-20;
-                     money=money+7;
-                     milk=milk-75; 
-                   cup=cup-1;
-                  }
-              }
-              
-                  else
-                  {
-                       if(water<200 || bean<12 || milk<100)
-                  {
-                      System.out.println("sorry i have insufficient integrident");
-                  }
-                        else
-                  {
-                    
-                     water=water-200;
-                     bean=bean-12;
-                     money=money+6;
-                     milk=milk-100; 
-                   cup=cup-1;
-                  }
-              }
-          }
-         else if(action.equals("fill"))
-         {
-             System.out.println("Write how many ml of water do you want to add:");
+      case "exit" :
+         System.exit(0);
+           break;
+       case "take" :
+              System.out.println("I gave you $"+money);
+             money=0;
+             break;
+       case "fill" :
+              System.out.println("Write how many ml of water do you want to add:");
              int pq=sc.nextInt();
              water=water+pq;
              System.out.println("Write how many ml of milk do you want to add: ");
@@ -87,18 +49,89 @@ public class CoffeeMachine {
              System.out.println("Write how many disposable cups of coffee do you want to add:");
              pq=sc.nextInt();
              cup=cup+pq;
-         }
-         else
-         {
-             System.out.println("I gave you $"+money);
-             money=0;
-         }
-          
-    System.out.println("The coffee machine has");
-          System.out.println(water+" of water\n"+milk+" of milk\n"+bean+" of coffee beans\n"+cup+" of disposable cups\n"+money+" of money");
-    
+             break;
+          case "buy" :
+              System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ");
+              String num=sc.next();
+              if(num.equals("1"))
+              {
+                  if(water<250 || bean<16)
+                  {  System.out.print("sorry ,not enough ");
+                      if(water<250)
+                       System.out.print("water ");
+                       if(bean<16)
+                       System.out.print("and bean");
+                       System.out.println("!");
+                      
+                  }
+                  else
+                  {  System.out.println("I have enough resources, making you a coffee!");
+                     water=water-250;
+                     bean=bean-16;
+                     money=money+4; 
+                     cup=cup-1;
+                  }
+                  
+              break;}
+              else if(num.equals("2"))
+              {
+                  if(water<350 || bean<20 || milk<75)
+                  {
+                      System.out.print("sorry ,not enough ");
+                      if(water<350)
+                       System.out.print("water ");
+                       if(bean<20)
+                       System.out.print("and bean");
+                       if(milk<75)
+                       System.out.print("and milk");
+                       System.out.println("!");
+                  }
+                  else
+                  {
+                    System.out.println("I have enough resources, making you a coffee!");
+                     water=water-350;
+                     bean=bean-20;
+                     money=money+7;
+                     milk=milk-75; 
+                   cup=cup-1;
+                  }
+              break;}
+              
+                  else if(num.equals("3"))
+                  {
+                       if(water<200 || bean<12 || milk<100)
+                  {
+                      
+                      System.out.print("sorry ,not enough ");
+                      if(water<200)
+                       System.out.print("water ");
+                       if(bean<12)
+                       System.out.print("and bean");
+                       if(milk<100)
+                       System.out.print("and milk");
+                       System.out.println("!");
+                  }
+                        else
+                  {
+                    System.out.println("I have enough resources, making you a coffee!");
+                     water=water-200;
+                     bean=bean-12;
+                     money=money+6;
+                     milk=milk-100; 
+                   cup=cup-1;
+                  }
+              break;}
+              else
+              {
+                  break;
+              }
+              default :
+                 System.out.println("Plz enter valid action");
+          }
+        
     
     
     }
     
+}
 }
